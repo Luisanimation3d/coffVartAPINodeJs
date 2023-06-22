f6fd682785fb1f2e92df521906882a51a20732dd
AlejandroVelasHel <velalejandro23@gmail.com>
2023-06-22 17:33:43 -0500

    se agregaron las rutas en el server

--- models/server.js
+++ models/server.js
@@ -8,6 +8,9 @@ class Server {
 		this.port = process.env.PORT || 3000;
 
 		this.usersPath = '/api/user';
+		this.contractsPath = '/api/contract';
+		this.paysPath = '/api/pay';
+		this.productionsPath = '/api/production';
 
 		// middleware
 		this.middlewares();
@@ -37,6 +40,9 @@ class Server {
 
 	routes() {
 		this.app.use(this.usersPath, require('../routes/UserRoute'));
+		this.app.use(this.contractsPath, require('../routes/contractRoute'));
+		this.app.use(this.paysPath, require('../routes/payRoute'));
+		this.app.use(this.productionsPath, require('../routes/productionRoute'));
 	}
 
 	async conectarDb() {
